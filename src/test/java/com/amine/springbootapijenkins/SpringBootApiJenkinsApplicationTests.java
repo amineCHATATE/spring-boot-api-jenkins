516c25a6d3b592b5db2f0b6108913089c8f577ed
Amine Chatate <amine.chatate@gmail.com>
2022-09-01 09:10:07 +0100

    test order

--- src/test/java/com/amine/springbootapijenkins/SpringBootApiJenkinsApplicationTests.java
+++ src/test/java/com/amine/springbootapijenkins/SpringBootApiJenkinsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.amine.springbootapijenkins;
 
-import org.junit.jupiter.api.AfterAll;
+import com.amine.springbootapijenkins.entity.Theme;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith( SpringExtension.class)
@@ -22,6 +30,7 @@ class SpringBootApiJenkinsApplicationTests {
     private MockMvc mockMvc;
 
     @Test
+    @Order(1)
     public void getsAllThemes() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/theme")
                         .accept(MediaType.APPLICATION_JSON))
@@ -30,6 +39,7 @@ class SpringBootApiJenkinsApplicationTests {
     }
 
     @Test
+    @Order(2)
     public void getsSingleTheme() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/theme/1")
                         .accept(MediaType.APPLICATION_JSON))
@@ -38,6 +48,7 @@ class SpringBootApiJenkinsApplicationTests {
     }
 
     @Test
+    @Order(3)
     public void returnsNotFoundForInvalidSingleTheme() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/theme/50")
                         .accept(MediaType.APPLICATION_JSON))
@@ -46,6 +57,7 @@ class SpringBootApiJenkinsApplicationTests {
     }
 
     @Test
+    @Order(4)
     public void addsNewTheme() throws Exception {
         String newTheme = "{\"name\":\"Monorail\",\"description\":\"Sedate travelling ride.\"}";
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/theme")
@@ -57,7 +69,6 @@ class SpringBootApiJenkinsApplicationTests {
     }
 
     @Test
-    @AfterAll
     public void deleteTheme() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/theme/1")
                         .accept(MediaType.APPLICATION_JSON))

93f3ebe8773141609b56d1bc4ada289de1f0ddb1
Amine Chatate <amine.chatate@gmail.com>
2022-09-01 11:00:30 +0100

    adding dockerfile

--- src/test/java/com/amine/springbootapijenkins/SpringBootApiJenkinsApplicationTests.java
+++ src/test/java/com/amine/springbootapijenkins/SpringBootApiJenkinsApplicationTests.java
@@ -1,7 +1,9 @@
 package com.amine.springbootapijenkins;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ExtendWith( SpringExtension.class)
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SpringBootApiJenkinsApplicationTests {
 
     @Autowired
